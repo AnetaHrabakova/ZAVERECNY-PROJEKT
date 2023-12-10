6e4eaf25a3a3f1fc68489f517c4fd406cd6d85ab
Aneta Hrabakova <anne.bakova@gmail.com>
2023-12-10 22:53:12 +0100

    instalace eleventy - druhy pokus

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
 module.exports = function(eleventyConfig) {
-    
-    
-    eleventyConfig.addPassthroughCopy("images");
-   
-  
-    eleventyConfig.addPassthroughCopy("css");
 
-    return {
-       
-        templateFormats: ["njk", "html", "md"],
-        markdownTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
-        dataTemplateEngine: "njk",
-      }
-    
-   };
-   
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat images/ do _site/images
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat css/ to _site/css/
+	eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk", "html", "md", "liquid"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
